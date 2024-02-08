c59eda9d8ad93d787b97f0ebd12576582f535644
spark-02 <>
2024-02-08 14:51:49 +0900

    途中コミット

--- Weather/Weather/ViewController.swift
+++ Weather/Weather/ViewController.swift
@@ -21,11 +21,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        navigationItem.title = allArea?.area.rawValue //rawValueは配列の中の情報全部持ってきてくれる
-        /* if let areaName = self?.area {
-        navigationItem.title = areaName.rawValue
-         }
-    */
+        navigationItem.title = allArea?.area.rawValue
         
         reloadIndicate.hidesWhenStopped = true
         
